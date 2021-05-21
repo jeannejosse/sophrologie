6cf3671d05cf91b43078125e962d240c2e4da705
jeannejosse <jeannejosse@gmail.com>
2021-05-21 12:36:48 +0200

    clean js + navbar all pages

--- main.js
+++ main.js
@@ -1,81 +1,37 @@
+const toggleClasses = function (selector, oldClass, newClass) {
+  document.querySelectorAll(selector).forEach((element) => {
+    element.classList.remove(oldClass);
+    element.classList.add(newClass);
+  })
+};
+
 window.addEventListener('scroll',function(e){
   if (window.innerWidth > 1024) {
     if(window.scrollY < 72) {
       document.querySelector(".dropdown-content-large-screen").style.backgroundColor ="transparent";
       document.querySelector(".dropdown-content-large-screen").style.color ="white";
-      // console.log('nok');
+
     } else {
-      console.log(document.querySelector(".dropdown-content"));
+      // console.log(document.querySelector(".dropdown-content"));
       document.querySelector(".dropdown-content-large-screen").style.backgroundColor ="white";
       document.querySelector(".dropdown-content-large-screen").style.color ="black";
     }
     if(window.scrollY < 176) {
-      // document.querySelector(".site-description").style.position = "relative";
-      // document.querySelector(".site-description").style.top = "30px";
-      // document.querySelector("nav").style.position = "relative";
-      // document.querySelector("nav").style.top = "30px";
-      document.querySelector("header").style.marginTop = "30px";
+      toggleClasses("header", "scrolled", "not-scrolled");
 
-      document.querySelector("header").style.background = "transparent";
+      toggleClasses("nav .active-scrolled", "active-scrolled", "active");
 
-      document.querySelector("nav .active").style.color = "#9CC5C7";
-      document.querySelector("nav .active").style.borderBottom ="solid 2px #9CC5C7";
-
-
-      for (let i in document.querySelectorAll("nav a.underline")){
-        document.querySelectorAll("nav a.underline")[i].addEventListener("mouseover",function (){
-          document.querySelectorAll("nav a.underline")[i].style.color ="#9CC5C7";
-          document.querySelectorAll("nav a.underline")[i].style.borderBottom ="solid 2px #9CC5C7";
-        });
-
-        document.querySelectorAll("nav a.underline")[i].addEventListener("mouseout",function (){
-          document.querySelectorAll("nav a.underline")[i].style.color ="#fff";
-          //document.querySelectorAll(".nav a.underline")[i].style.borderBottom ="solid 2px #9CC5C7";
-        });
-      }
+      toggleClasses("a.underline-scrolled", "underline-scrolled", "underline");
 
     } else {
-      // Note: we could implement a loop to iterate over querySelectorAll matches (Nodelist)
-      // document.querySelector(".site-description").style.position = "relative";
-      // document.querySelector(".site-description").style.top = "0px";
-      // document.querySelector("nav").style.position = "relative";
-      // document.querySelector("nav").style.top = "0px";
-      document.querySelector("header").style.marginTop = "0px";
+      toggleClasses("header", "not-scrolled", "scrolled");
 
-      document.querySelector("header").style.background = "rgba(166, 203, 204,0.90)";
+      toggleClasses("nav .active", "active", "active-scrolled");
 
-      document.querySelector("nav .active").style.color = "#000";
-      document.querySelector("nav .active").style.borderBottom ="solid 2px #000";
-
-
-
-      for (let i in document.querySelectorAll("nav a.underline")){
-        document.querySelectorAll("nav a.underline")[i].addEventListener("mouseover",function (){
-          document.querySelectorAll("nav a.underline")[i].style.color ="#000";
-          document.querySelectorAll("nav a.underline")[i].style.borderBottom ="solid 2px #000";
-        });
-
-        document.querySelectorAll("nav a.underline")[i].addEventListener("mouseout",function (){
-          document.querySelectorAll("nav a.underline")[i].style.color ="#fff";
-          document.querySelectorAll("nav a.underline")[i].style.borderBottom ="none";
-        });
-      }
+      toggleClasses("a.underline", "underline", "underline-scrolled");
     }
   } else {
-      document.querySelector("header").style.background = "#9CC5C7";
-      document.querySelector("header").style.marginTop = "0px";
-
-    //   document.querySelector(".sub-menu-trigger").addEventListener("click", function() {
-    //   document.querySelectorAll(".sub-menu").forEach(subMenu => {
-    //     if (subMenu.style.display === "none") {
-    //       subMenu.style.display = "block";
-    //       document.querySelector(".menu-arrow").innerHTML = "▲"
-    //     } else {
-    //       subMenu.style.display = "none";
-    //       document.querySelector(".menu-arrow").innerHTML = "▼"
-    //     }
-    //   });
-    // });
+      toggleClasses("header", "not-scrolled", "scrolled");
   }
 });
 
@@ -149,7 +105,6 @@ document.addEventListener('scroll', (e) => {
   if (scrolled > scrollBefore) {
 
     scrollBefore = scrolled;
-
     document.querySelectorAll(".appear").forEach((element) => {
       // console.log(element);
       const elementBottomFromWindowBottom = element.getBoundingClientRect().bottom - window.innerHeight;
